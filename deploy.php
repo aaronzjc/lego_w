aea98a3aa22a52b857f18d1cafb37937f41dbb17
JinchengZhang <jincheng3@staff.weibo.com>
2018-12-19 19:25:36 +0800

    deployer

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'lego_w');
+
+set('http_user', 'nginx');
+
+// Project repository
+set('repository', 'git@github.com:aaronzjc/lego_w.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+// Hosts
+
+host("tx")
+    ->user("memosa")
+    ->configFile('~/.ssh/config')
+    ->identityFile('~/.ssh/tx_rsa')
+    ->forwardAgent(true)
+    ->multiplexing(true)
+    ->set("deploy_path", "/var/www/html/{{ application }}"); 
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:cache:clear',
+    'artisan:optimize',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
